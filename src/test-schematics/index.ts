ccc14be1b00173cc38b1492014c84932f66db61c
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-22 14:46:12 +0800

    feat: _options use schema

--- src/test-schematics/index.ts
+++ src/test-schematics/index.ts
@@ -3,8 +3,12 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function testSchematics(_options: any): Rule {
+export function testSchematics(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
+}
+
+export interface Schema {
+  name: string
 }
