fb09d9a88bd8ceb0c7f5acaeb32b3430333fd534
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-22 14:46:38 +0800

    feat: create a file

--- src/test-schematics/index.ts
+++ src/test-schematics/index.ts
@@ -5,6 +5,16 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function testSchematics(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const { name } = _options;
+    const datetime = new Date();
+    const now =
+        datetime.getFullYear().toString() + '-'
+        + (datetime.getMonth() + 1).toString() + '-'
+        + datetime.getDate().toString() + '-'
+        + datetime.getHours().toString() + '_'
+        + datetime.getMinutes().toString() + '_'
+        + datetime.getSeconds().toString();
+    tree.create(`test-${now}.js`, `console.log('Hello ${name}');`);
     return tree;
   };
 }
