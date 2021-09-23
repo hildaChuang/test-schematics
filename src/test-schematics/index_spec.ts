65fee5753c14e218dbe896f1961512bff599871f
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-23 11:59:41 +0800

    refactor: unit test for dasherize file name

--- src/test-schematics/index_spec.ts
+++ src/test-schematics/index_spec.ts
@@ -1,27 +1,20 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
+import { strings } from "@angular-devkit/core";
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('test-schematics', () => {
-  const expectResult = async (fileName?: string) => {
-    const fullFileName = `/${fileName || 'default'}`;
-    const params = fileName ? {name: fileName} : {};
+  it('成功產出檔案，檔名為"/hello-hilda-chuang.component.ts"', async () => {
+    const name = 'hildaChuang';
     const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = await runner
-      .runSchematicAsync('test-schematics', params, Tree.empty())
+      .runSchematicAsync('test-schematics', {name: name}, Tree.empty())
       .toPromise();
 
+    const dasherizeName = strings.dasherize(name);
+    const fullFileName = `/hello-${dasherizeName}.component.ts`;
     expect(tree.files).toContain(fullFileName);
-    expect(tree.readContent(fullFileName)).toEqual('hello');
-  }
-
-  it('沒給檔名，檔名為 default，內容為 "hello"', async () => {
-    expectResult();
-  });
-
-  it('給檔名，檔名為 Hilda，內容為 "hello"', async () => {
-    expectResult('Hilda');
   });
 });
