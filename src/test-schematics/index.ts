8d55630ca29eac064e3a2b29770a56bec05fa42a
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-23 10:28:38 +0800

    feat: use template to create file

--- src/test-schematics/index.ts
+++ src/test-schematics/index.ts
@@ -1,12 +1,18 @@
-import {Rule, SchematicContext,Tree} from '@angular-devkit/schematics';
+import { apply, mergeWith, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function testSchematics(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    tree.create(_options.name || 'default', 'hello');
-    return tree;
+    // 使用範本
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options, // 使用者所輸入的參數
+      })
+    ]);
+    return mergeWith(sourceParametrizedTemplates)(tree, _context);
   };
 }
 
