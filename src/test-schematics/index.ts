8b1b88c7e0538a61dcbedad5874ad2b95869456a
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-23 11:13:37 +0800

    refactor: use strings helper function

--- src/test-schematics/index.ts
+++ src/test-schematics/index.ts
@@ -1,4 +1,6 @@
 import { apply, mergeWith, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
+// 引入 strings ，所有的字串處理函式都在裡面
+import { strings } from "@angular-devkit/core";
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
@@ -10,6 +12,7 @@ export function testSchematics(_options: Schema): Rule {
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
         ..._options, // 使用者所輸入的參數
+        ...strings,  // 將這些函式加到規則裡，範本語法才能正常運作
       })
     ]);
     return mergeWith(sourceParametrizedTemplates)(tree, _context);
