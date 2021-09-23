b728e89011f9fd247719c93fbe41e238f80fe3eb
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-23 09:28:46 +0800

    feat: file content test

--- src/test-schematics/index_spec.ts
+++ src/test-schematics/index_spec.ts
@@ -27,4 +27,27 @@ describe('test-schematics', () => {
 
     expect(tree.files).toContain(`/${fileName}`);
   });
+
+  it('not set file name - content is hello', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner
+      .runSchematicAsync('test-schematics', {}, Tree.empty())
+      .toPromise();
+
+    expect(tree.readContent('/default')).toEqual('hello');
+  });
+
+  it('set file name -  content is hello', async () => {
+    const fileName = 'Hilda';
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner
+      .runSchematicAsync(
+        'test-schematics',
+        { name: fileName },
+        Tree.empty()
+      )
+      .toPromise();
+
+    expect(tree.readContent(`/${fileName}`)).toEqual('hello');
+  });
 });
