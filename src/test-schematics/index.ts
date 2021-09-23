dd43d38a53b180ea21cfa4c4e9253aa01f23110a
hilda.chuang <hilda.chuang@cimforce.com>
2021-09-23 09:03:39 +0800

    refactor: create a file

--- src/test-schematics/index.ts
+++ src/test-schematics/index.ts
@@ -1,19 +1,12 @@
-import {apply, mergeWith, Rule, SchematicContext, template, Tree, url} from '@angular-devkit/schematics';
-import {strings} from "@angular-devkit/core";
+import {Rule, SchematicContext,Tree} from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function testSchematics(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    const sourceTemplate = url('./files');
-    const sourceParamsTemplate = apply(sourceTemplate, [
-        template({
-          ..._options,
-          ...strings,
-        })
-    ]);
-    return mergeWith(sourceParamsTemplate)(tree, _context);
+    tree.create(_options.name || 'default', 'hello');
+    return tree;
   };
 }
 
