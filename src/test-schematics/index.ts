e40ef35f362df868b5c8259aa4e0e0fc98c53736
hilda.chuang <hilda.chuang@cimforce.com>
2024-07-01 17:13:40 +0800

    feat(day6): 使用範本結合 schema.json name 參數，建立檔案

--- src/test-schematics/index.ts
+++ src/test-schematics/index.ts
@@ -1,24 +1,25 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { apply, mergeWith, Rule, SchematicContext, template, Tree, url } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function testSchematics(_options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-
+  return (_tree: Tree, _context: SchematicContext) => {
     // 產生一個檔案名稱為使用者所輸入的 --name 參數的值，沒輸入則為 'hello' 的檔案
     // 檔案內容為 'world'
-    tree.create(_options.name || 'hello', 'world');
-    return tree;
+    // tree.create(_options.name || 'hello', 'world');
+    // return tree;
 
-    // const sourceTemplate = url('./files');
-    // const sourceParamsTemplate = apply(sourceTemplate, [
-    //     template({
-    //       ..._options,
-    //       ...strings,
-    //     })
-    // ]);
-    // return mergeWith(sourceParamsTemplate)(tree, _context);
+    // 設定使用的範本位置
+    const sourceTemplates = url('./files');
+    const sourceParametrizedTemplate = apply(sourceTemplates, [
+      template({
+        // 使用者輸入的參數 name 取代範本檔名的 '__name__'
+        ..._options,
+        // ...strings,
+      })
+    ]);
+    return mergeWith(sourceParametrizedTemplate);
   };
 }
 
