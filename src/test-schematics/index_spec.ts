4e763b1cc17e1b16bdc0097a81de0ced8330dbcd
hilda.chuang <hilda.chuang@cimforce.com>
2024-07-01 13:47:26 +0800

    feat(day4): first schematic test

--- src/test-schematics/index_spec.ts
+++ src/test-schematics/index_spec.ts
@@ -5,12 +5,20 @@ import * as path from 'path';
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('test-schematics', () => {
-  it('works', async () => {
+  const expectResult = async (fileName?: string) => {
+    const fullFileName = `/${fileName || 'hello'}`;
+    const params = fileName ? {name: fileName} : {};
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner
-      .runSchematic('test-schematics', {}, Tree.empty())
-        .then();
+    const tree = await runner.runSchematic('test-schematics', params, Tree.empty()).then();
+    expect(tree.files).toContain(fullFileName);
+    expect(tree.readContent(fullFileName)).toEqual('world');
+  }
 
-    expect(tree.files).toEqual([]);
+  it('沒有給檔名，則檔名為 "hello"，檔案內容為 "world"', async () => {
+    expectResult();
+  });
+
+  it('有給檔名，則檔名為指定的 "test-file-name-given"，檔案內容為 "world"', async () => {
+    expectResult('test-file-name-given');
   });
 });
